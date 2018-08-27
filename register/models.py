a064c8af8c9be98552a4e83d00414c30e1688798
Jacob <jacobtamus@gmail.com>
2018-08-26 18:11:38 -0600

    created the User and Event Table in the database

--- register/models.py
+++ register/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from events.models import Events
 # Create your models here.
 class User(models.Model):
-    email = models.CharField(null=True, unique=True, primary_key=True, max_length=200)
+    email = models.CharField(unique=True, primary_key=True, max_length=200)
     userID = models.ForeignKey(Events, on_delete=models.CASCADE)
     date_joined = models.DateTimeField(auto_now_add=True)
     password = models.CharField(null=True, unique=True, max_length=200)
